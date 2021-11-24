db18d2fb44e6a5008fb902d76d9838cb839460cb
Bahulya <sarvalabahulya456@gmail.com>
2021-11-24 23:50:23 +0530

    BinaryTree

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿namespace BinaryTreeUC2
+{
+    public class Node
+    {
+
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+
+    }
+}
